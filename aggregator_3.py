e5c2988eb210b13885e3de6e552edf3babec8058
Nicola Gulmini <nicolagulmini@gmail.com>
2021-09-04 17:16:55 +0200

    change aggregator structure

--- aggregator_3.py
+++ aggregator_3.py
@@ -11,18 +11,20 @@ from numpy import swapaxes
 from random import randint
 
 class aggregator():
-# consider also the local models weights!
+# consider also the local models weights
     
     def __init__(self, number_of_clusters):
         image = Input(shape=(28, 28), name="input_image") 
         clusters_weights = Input(shape=(number_of_clusters, 784, 10), name='input_cluster_model_weights')
         
         flatten_image = Flatten()(image)
+        dense_flatten_img = Dense(30, activation='relu')(flatten_image)
         flatten_clusters_weights = Flatten()(clusters_weights)
+        dense_flatten_clus_w = Dense(10*number_of_clusters, activation='relu')(flatten_clusters_weights)
+        concat = Concatenate()([dense_flatten_img, dense_flatten_clus_w])
         # other dense layers?
-        concat = Concatenate()([flatten_image, flatten_clusters_weights])
-        # other dense layers?
-        y = Dense(10, activation='softmax')(concat)
+        y = Dense(100, activation='relu')(concat)
+        y = Dense(10, activation='softmax')(y)
 
         model = Model(inputs=[image, clusters_weights], outputs=y, name='aggregator_3')
         opt = Adam(learning_rate = 0.001)
