84b87328cfcc34281aaf5e587a592d3b35a4029e
Nicola Gulmini <nicolagulmini@gmail.com>
2021-09-07 15:31:52 +0200

    Update aggregator_3.py

--- aggregator_3.py
+++ aggregator_3.py
@@ -27,11 +27,20 @@ class aggregator():
         model.compile(optimizer=opt, loss='categorical_crossentropy', metrics='accuracy')
         self.model = model
 
-    def evaluation(self, fed_scenario, x_test, y_test):
-        return 0
+class attention_based_aggregator():
     
-    def local_evaluation(self, fed_scenario):
-        return 0
-    
-    def custom_x(self):
-        return 0
+    def __init__(self, number_of_clusters):
+        image = Input(shape=(28, 28), name="input_image") 
+        cluster_outputs = Input(shape=(number_of_clusters, 10), name='softmax_outputs')
+        
+        flatten_image = Flatten()(image)
+        #flatten_cluster_outputs = Flatten()(cluster_outputs)
+        weights = Dense(number_of_clusters, activation='sigmoid')(flatten_image) # or tanh or softmax
+        #concat = Concatenate()([flatten_image, flatten_clusters_weights])
+        # take the sum
+        #y = Dense(10, activation='softmax')(concat)
+
+        model = Model(inputs=[image, cluster_outputs], outputs=y, name='attention_based_aggregator')
+        opt = Adam(learning_rate = 0.001)
+        model.compile(optimizer=opt, loss='categorical_crossentropy', metrics='accuracy')
+        self.model = model
