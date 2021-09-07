9cc63567e7c760c163ef5b2b0618f42aa9ef74ba
Nicola Gulmini <nicolagulmini@gmail.com>
2021-09-07 22:31:26 +0200

    Update aggregator_3.py

--- aggregator_3.py
+++ aggregator_3.py
@@ -64,12 +64,9 @@ class attention_based_aggregator():
         image = Input(shape=(28, 28), name="input_image") 
         cluster_outputs = Input(shape=(number_of_clusters, 10), name='softmax_outputs')
         flatten_image = Flatten()(image)
-        flatten_image = Dense(100)(flatten_image)
-        flatten_image = Flatten()(flatten_image)
         weights = Dense(number_of_clusters, activation='softmax')(flatten_image) # sigmoid or tanh or softmax
         summ = Dot(axes=1)([weights, cluster_outputs])
-        y = Dense(100, activation='relu')(summ)
-        y = Dense(10, activation='softmax')(y)
+        y = Dense(10, activation='softmax')(summ)
         
         model = Model(inputs=[image, cluster_outputs], outputs=summ, name='attention_based_aggregator')
         opt = Adam(learning_rate = 0.001)
