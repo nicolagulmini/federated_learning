498378c12c00122ff305a3d29f047275871b519b
Nicola Gulmini <nicolagulmini@gmail.com>
2021-09-07 18:01:51 +0200

    comment for colab tutorial

--- aggregator_3.py
+++ aggregator_3.py
@@ -27,6 +27,33 @@ class aggregator():
         opt = Adam(learning_rate = 0.001)
         model.compile(optimizer=opt, loss='categorical_crossentropy', metrics='accuracy')
         self.model = model
+    ''' Aggregator 3 colab section
+    from keras.models import Model
+    # layers[1] is the intermediate dense layer
+    intermediate_models = [Model(inputs=fed_setup.list_of_clusters[i].get_model().input,
+                                     outputs=fed_setup.list_of_clusters[i].get_model().layers[1].output) for i in range(len(fed_setup.list_of_clusters))]
+
+    # server aggregator perf only at the end
+    server_agg = aggregator(len(fed_setup.list_of_clusters)) # <class 'federated_learning.aggregator_3.aggregator'>
+    server_epochs = 30
+    server_x_train, server_y_train, server_x_val, server_y_val = fed_setup.train_validation_split(fed_setup.server.x_train, fed_setup.server.y_train)
+    outputs = [model.predict(server_x_train) for model in intermediate_models]
+    val_outputs = [model.predict(server_x_val) for model in intermediate_models]
+    test_outputs = [model.predict(fed_setup.server.x_test) for model in intermediate_models]
+
+    history = server_agg.model.fit(
+        x=[server_x_train, array([array(outputs)]).reshape((len(server_x_train), 9, 10))],
+        y=server_y_train,
+        batch_size=32, 
+        epochs=server_epochs, 
+        verbose=0, 
+        validation_data=([server_x_val, array([array(val_outputs)]).reshape((len(server_x_val), 9, 10))], server_y_val),
+        shuffle=True    
+        )
+
+    # measure the global performance
+    server_aggregator_performance = server_agg.model.evaluate([fed_setup.server.x_test, array([array(test_outputs)]).reshape((len(fed_setup.server.x_test), 9, 10))], fed_setup.server.y_test, verbose=0)[1]
+    '''
 
 class attention_based_aggregator():
     
