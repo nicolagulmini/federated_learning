324875024ec9c6f8d65154842f6c3a2f6a69f6a9
Nicola Gulmini <62892813+nicolagulmini@users.noreply.github.com>
2021-09-30 19:12:46 +0200

    Update aggregator_3.py

--- aggregator_3.py
+++ aggregator_3.py
@@ -66,9 +66,9 @@ class attention_based_aggregator():
         image = Input(shape=(28, 28), name="input_image") 
         cluster_outputs = Input(shape=(number_of_clusters, 10), name='softmax_outputs')
         flatten_image = Flatten()(image)
-        weights = Dense(number_of_clusters, activation='softmax', bias_initializer=Ones(), kernel_initializer=Ones())(flatten_image)
+        weights = Dense(number_of_clusters, activation='softmax')(flatten_image)
         out = Dot(axes=1)([weights, cluster_outputs])
-        out = Dense(10, activation='softmax', bias_initializer=Ones(), kernel_initializer=Ones())(out)
+        out = Dense(10, activation='softmax')(out)
         model = Model(inputs=[image, cluster_outputs], outputs=out, name='attention_based_aggregator')
         model.compile(optimizer=Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics='accuracy')
         self.model = model
