40f92c97fe3c7422643500bf91761806546ce7b1
Nicola Gulmini <62892813+nicolagulmini@users.noreply.github.com>
2021-10-03 18:34:35 +0200

    Update aggregator_3.py

--- aggregator_3.py
+++ aggregator_3.py
@@ -71,9 +71,9 @@ class attention_based_aggregator():
         image = Input(shape=(28, 28), name="input_image") 
         cluster_outputs = Input(shape=(number_of_clusters, 10), name='softmax_outputs')
         flatten_image = Flatten()(image)
-        weights = Dense(number_of_clusters, activation='linear', bias_initializer=Ones(), kernel_initializer=Zeros())(flatten_image) #activation='softmax', bias_initializer=Zeros(), kernel_initializer=Zeros()
+        weights = Dense(number_of_clusters, activation='linear', bias_initializer=Ones(), kernel_initializer=RandomNormal(mean=0.0, stddev=0.05, seed=None))(flatten_image) #activation='softmax', bias_initializer=Zeros(), kernel_initializer=Zeros()
         out = Dot(axes=1)([weights, cluster_outputs])
-        out = Dense(10, activation='linear', kernel_initializer=Identity(), bias_initializer=Zeros())(out)
+        out = Dense(10, activation='softmax', kernel_initializer=Identity(), bias_initializer=RandomNormal(mean=0.0, stddev=0.05, seed=None))(out)
         model = Model(inputs=[image, cluster_outputs], outputs=out, name='attention_based_aggregator')
         model.compile(optimizer=Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics='accuracy')
         self.model = model
