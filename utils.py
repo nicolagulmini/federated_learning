40555a7d89e5c1bed6652184feac839e135f6f63
Nicola Gulmini <62892813+nicolagulmini@users.noreply.github.com>
2021-11-05 09:23:46 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -361,6 +361,7 @@ class federated_setup:
         for _ in range(number_of_server_training_data):
             tmp_index = randint(0, len(original_mnist_x_train)-1)
             if original_mnist_y_train[tmp_index][0] < number_of_classes:
+                print('original_mnist_y_train[tmp_index][0]', original_mnist_y_train[tmp_index][0])
                 server_x_train.append(transform.rotate(original_mnist_x_train[tmp_index], choice([0, 90, 180, 270])))
                 server_y_train.append(original_mnist_y_train[tmp_index][0])
             
@@ -369,7 +370,6 @@ class federated_setup:
             if original_mnist_y_train[tmp_index][0] < number_of_classes:
                 server_x_test.append(transform.rotate(original_mnist_x_test[tmp_index], choice([0, 90, 180, 270])))
                 server_y_test.append(original_mnist_y_test[tmp_index][0])
-        print(server_y_test) # focus
                 
         server_y_test = to_categorical(server_y_test, number_of_classes)
         server_y_train = to_categorical(server_y_train, number_of_classes)
