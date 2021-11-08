f67c299ec5085b908fe649e8815eac19a7a43c6a
Nicola Gulmini <62892813+nicolagulmini@users.noreply.github.com>
2021-11-08 16:21:50 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -144,7 +144,7 @@ class user_information:
             print("Accuracy of the user " + str(self.name) + " of the cluster " + str(self.cluster.number) + " BEFORE the training is " + str(accuracy))
         
         # training
-        self.model.fit(x_train, y_train, epochs=epochs, batch_size=batch, verbose=0, validation_data=(x_val, y_val), shuffle=True, callbacks=[EarlyStopping(monitor='categorical_crossentropy', patience=3)])
+        self.model.fit(x_train, y_train, epochs=epochs, batch_size=batch, verbose=0, validation_data=(x_val, y_val), shuffle=True, callbacks=[EarlyStopping(monitor='loss', patience=3)])
 
         if not verbose == 0:
             accuracy = self.model.evaluate(self.cluster.test_data['images'], to_categorical(self.cluster.test_data['labels'], number_of_classes))[1]
